14296fc83e1d02879e91a7a3ff958b7f8453d82a
Michal Gaj <michalgaj@onet.pl>
2018-01-27 11:43:42 +0100

    [Config] Add configuration file

---
+++ config/config.h
@@ -0,0 +1,10 @@
+//
+// Created by Michal on 1/26/2018.
+//
+namespace CurrencyInvestingProfitability
+{
+    struct address {
+        unsigned short port = 7000;
+        const char *address = "127.0.0.1";
+    } serverAddress;
+}
